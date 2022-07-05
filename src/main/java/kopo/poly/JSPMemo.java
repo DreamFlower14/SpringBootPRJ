a7a955a3e9c6db26e98eaef21d984ecfdaf16a5a
data23 <sunghh1004@naver.com>
2022-07-05 15:28:13 +0900

    2022-07-05

---
+++ src/main/java/kopo/poly/JSPMemo.java
@@ -0,0 +1,25 @@
+package kopo.poly;
+
+public class JSPMemo {
+/*
+     자바스프링 순서
+    URL 호출 -> Controller(자바로 만듦) -> Service(자바로 만듦) -> Mapper(자바와 XML 로 만듦) -> DB(오라클)
+    JSP <- Controller(자바로 만듦) <- Service(자바로 만듦) <- Mapper(자바와 XML 로 만듦) <- DB(오라클)
+
+    Controller : URL 에서 오는 모든 내용을 받음
+                EX) 로그인 할 때 ID PASS 친 다음에 엔터를 치면 데이터가 Controller 로 감
+    Service : 기능을 처리
+    Mapper :
+    DB : 데이터저장소소
+    DTO : 자바 파일에서 자바 파일로 넘어갈 때 편하게 해줌
+        메서드를 쓸 때 메서드 안에 매개변수를 클래스 형태로 저장해두고 SUM (DTO) 같이 쓸 수 있음
+        EX) SUM(int a, int b, int c)
+            => DTO 안에 (int a, int b, int c) 저장
+                => SUM(DTO)
+    ibatis : xml 를 통해서 sql 를 사용할 수 있게한다.
+
+
+
+
+*/
+}
--- src/main/webapp/WEB-INF/views/form.jsp
+++ src/main/webapp/WEB-INF/views/form.jsp
@@ -16,7 +16,7 @@
         <h1>Form</h1>
         <p>폼을 통해 데이터 전송해봅시다.</p>
     </div>
-    <form action="/getNoticeData" method="post">
+    <form action="/getInsertNotice" method="get">
         <div class="form-group">
             <label>작성자</label>
             <input name="reg_id" type="text" class="form-control"/>
